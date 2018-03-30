9effa9451830e3f767e793f85f2825e1cd9c2810
Michelle Olivieri <michelleo72@gmail.com>
2018-03-30 21:38:24 +0000

    automatically backed up by learn

---
+++ lib/fancy_dance.rb
@@ -0,0 +1,15 @@
+module FancyDance
+  module InstanceMethods
+    
+
+
+
+end
+
+  module Class Methods
+
+    def metadata
+      "This class produces objects that love to dance."
+    end
+  end
+end
